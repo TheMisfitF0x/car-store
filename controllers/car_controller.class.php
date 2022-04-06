af89469422ae9d0329770043d6d327030a7b7b4c
Aero48 <56200404+Aero48@users.noreply.github.com>
2022-04-06 13:56:19 -0400

    added basic structure to car_controller class

--- controllers/car_controller.class.php
+++ controllers/car_controller.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Authors: Isaac Lowe & Logan Douglass
+ * Authors: Logan Douglass
  * Date: 4/5/2022
  * File: car_controller.class.php
  * Description:
@@ -8,5 +8,23 @@
 
 class CarController
 {
+    private $car_model;
 
+    //default constructor
+    public function __construct() {
+        //create an instance of the MovieModel class
+        //$this->car_model = CarModel::getCarModel();
+    }
+
+    public function index(){
+
+    }
+
+    public function detail($id){
+
+    }
+
+    public function error($message){
+
+    }
 }
