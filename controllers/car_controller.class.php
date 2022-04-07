12d0915fd6d8f5433cc03dd5c86e0d002eb502ea
Aero48 <56200404+Aero48@users.noreply.github.com>
2022-04-06 21:22:30 -0400

    Added draft content to car_controller

--- controllers/car_controller.class.php
+++ controllers/car_controller.class.php
@@ -13,18 +13,45 @@ class CarController
     //default constructor
     public function __construct() {
         //create an instance of the MovieModel class
-        //$this->car_model = CarModel::getCarModel();
+        $this->car_model = CarModel::getCarModel();
     }
 
     public function index(){
+        //retrieve all cars and store them in an array
+        $cars = $this->car_model->list_car();
 
+        if (!$cars) {
+            //display an error
+            $message = "There was a problem displaying cars.";
+            $this->error($message);
+            return;
+        }
+
+        // display all movies
+        $view = new CarIndex();
+        $view->display($cars);
     }
 
-    public function detail($id){
+    public function detail($vin){
+        //retrieve the specific movie
+        $car = $this->car_model->view_car($vin);
 
+        if (!$car) {
+            //display an error
+            $message = "There was a problem displaying the vin='" . $vin . "'.";
+            $this->error($message);
+            return;
+        }
+
+        //display movie details
+        $view = new CarDetail();
+        $view->display($car);
     }
 
     public function error($message){
-
+        //create an object of the Error class
+        $error = new CarError();
+        //display the error page
+        $error->display($message);
     }
 }
