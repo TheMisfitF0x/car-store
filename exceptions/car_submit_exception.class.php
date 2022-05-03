02a59e9707b3e0540d1382fa7b92bffc5c38fb92
Aero48 <56200404+Aero48@users.noreply.github.com>
2022-05-03 17:55:20 -0400

    many more exceptions added

--- exceptions/car_submit_exception.class.php
+++ exceptions/car_submit_exception.class.php
@@ -1,6 +1,13 @@
 <?php
-
+/**
+ * Author: Logan Douglass
+ * Date: 5/3/2022
+ * Name: car_submit_exception.class.php
+ * Description: Creates a custom exception class to handle car submit errors
+ */
 class CarSubmitException extends Exception
 {
-
+    public function getOutput(){
+        return "There was a problem submitting the new car";
+    }
 }
