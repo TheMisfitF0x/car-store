8b6c09150c50296a2b6573952175bf8ff21bc11b
evminor1 <evminor@iu.edu>
2022-05-03 18:15:00 -0400

    About Complete (the page)

--- exceptions/invalid_login_exeption.class.php
+++ exceptions/invalid_login_exeption.class.php
@@ -7,7 +7,13 @@
  */
 class InvalidLoginException extends Exception
 {
-    public function getOutput(){
-        return "Error: Invalid username or password";
+    public function getOutput(){?>
+<div class="ListTitle" id="TitleBorder">
+    <div >
+        <h2 class="ListTitle" id="TitleText">Error: Invalid username or password</h2>
+    </div>
+</div>
+
+<?php
     }
-}
+}?>
--- views/about/about_index.class.php
+++ views/about/about_index.class.php
@@ -12,7 +12,18 @@ class AboutIndex extends IndexView{
         parent::displayHeader("About");
 
 ?>
-<p>hello</p>
+<div class="ListTitle" id="TitleBorder">
+    <div>
+        <h2 class="ListTitle" id="TitleText">About Ran's Racin Racer's</h2>
+    </div>
+</div>
+        <div class="viewCarsTable" id="CarViewBorder">
+            <div class="aboutRans">
+                <p class="aboutRans">    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp In the 1980's <strong>Ran</strong> started his adventure!  With a little help from his parents he was finally able to afford his own lot in <strong> Indianapolis!</strong> Ever since then <strong>Ran</strong> prides himself on selling the best <i>Unique</i> cars in the state!  We Challenge you to come find a better deal on your next automotive purchase!</p>
+            </div>
+            <img class="aboutRans" src="../../www/img/MercedesBenzMI.jpg">
+        </div>
+
 <?php
 
         parent::displayFooter();
